fd5352d6b83336de77ecfae1ac5e250583441752
angelLujan <pepe.lujan2@gmail.com>
2018-08-29 20:12:29 -0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('head')
+@endsection
+@section('scripts')
+@endsection
+@section('content')
+
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env('APP_NAME') }}</title>
+    @yield('head')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Our Team</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="main" id="app">
+    @section('content')
+    @endsection
+</div>
+@yield('scripts')
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
