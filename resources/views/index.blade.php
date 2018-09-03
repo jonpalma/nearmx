6159cc35759c5520d78f081bd66e72588dbf9705
angelLujan <pepe.lujan2@gmail.com>
2018-09-02 20:00:46 -0600

    First addition of styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,123 +1,192 @@
 @extends('layouts.master')
 @section('head')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 @section('scripts')
+    <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
 @endsection
 @section('content')
-    <div class="container" id="banner">
-        <h1>
-            We are creative
-            <span class="clearfix"></span>
-            Create Awesome Websites
-        </h1>
-        <h2>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
-        </h2>
-        <a href="#contact" rel="nofollow" class="btn btn-blue">Contact Us</a>
-    </div>
-    <div class="container" id="About">
-        <h1 class="heading">
-            <span class="text-light-gray">About our agency</span>
-            <span class="clearfix"></span>
-            Create Awesome Websites
-        </h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur.
-        </p>
-    </div>
-    <div class="container" id="services">
-        <h1 class="heading text-white">
-            Lorem ipsum
-            <span class="clearfix"></span>
-            Our Services
-        </h1>
-        <div class="row text-center">
-            <div class="col-6 col-sm-4">
-                <h3>Creative Design</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-            <div class="col-6 col-sm-4">
-                <h3>User Experience</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-            <div class="col-6 col-sm-4">
-                <h3>Custom</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-            <div class="col-6 col-sm-4">
-                <h3>Rating</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-            <div class="col-6 col-sm-4">
-                <h3>Responsive</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-            <div class="col-6 col-sm-4">
-                <h3>Support</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-                </p>
-            </div>
-        </div>
-        <a href="#contact" rel="nofollow" class="btn btn-blue">Contact Us</a>
-    </div>
-    <div class="container" id="portfolio">
-        <h1 class="heading">
-            <span class="text-light-gray">This is our work</span>
-            <span class="clearfix"></span>
-            Portfolio
-        </h1>
-        <button class="btn btn-blue">See more</button>
-    </div>
-    <div class="container" id="team">
-        <h1 class="heading">
-            <span class="text-white">Be part of the team</span>
-            <span class="clearfix"></span>
-            Our Team
-        </h1>
-    </div>
-    <div class="container" id="contact">
-        <h1 class="heading">
-            <span class="text-light-gray">Be part of the team</span>
-            <span class="clearfix"></span>
-            Our Team
-        </h1>
-        <div class="w-50">
-            {!! Form::open(['url' => '/']) !!}
-            <div class="w-50 float-left">
-                {!! Form::label('name','Full Name') !!}
-                {!! Form::text('name',null,['required' => 'required','placeholder' => 'Full Name']) !!}
-            </div>
-            <div class="w-50 float-left">
-                {!! Form::label('phone','Phone') !!}
-                {!! Form::tel('phone',null,['required' => 'required','placeholder' => '+52(614) 123 65 89']) !!}
-            </div>
-            <div>
-                {!! Form::label('mail','Email') !!}
-                {!! Form::email('mail',null,['required' => 'required','placeholder' => 'example@mail.com']) !!}
-            </div>
-            <div>
-                {!! Form::label('message','Message') !!}
-                {!! Form::textarea('message',null,['required' => 'required','placeholder' => 'Your message','rows' => '5']) !!}
-            </div>
-            <div>
-                {!! Form::submit('Send') !!}
-            </div>
-            {!! Form::close() !!}
+    <div id="banner">
+        <div class="container vertical-align text-center text-white">
+            <h1>
+                We are creative
+                <span class="clearfix"></span>
+                Create Awesome Websites
+            </h1>
+            <h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+            </h2>
+            <a href="#contact" rel="nofollow" class="btn btn-blue">Contact Us</a>
         </div>
     </div>
+    <section id="about">
+        <div class="container">
+            <h1 class="heading text-center">
+                <span class="text-light-gray">About our agency</span>
+                <span class="clearfix"></span>
+                Create Awesome Websites
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla pariatur.
+            </p>
+        </div>
+    </section>
+    <section id="services">
+        <div class="container text-center">
+            <h1 class="heading text-white">
+                Lorem ipsum
+                <span class="clearfix"></span>
+                Our Services
+            </h1>
+            <div class="row text-white">
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/1.png') }}" alt="Creative Design">
+                    <h5>Creative Design</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/2.png') }}" alt="User Experience">
+                    <h5>User Experience</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/3.png') }}" alt="Custom">
+                    <h5>Custom</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/4.png') }}" alt="Rating">
+                    <h5>Rating</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/5.png') }}" alt="Responsive">
+                    <h5>Responsive</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+                <div class="col-6 col-sm-4">
+                    <img src="{{ asset('img/services/6.png') }}" alt="Support">
+                    <h5>Support</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore.
+                    </p>
+                </div>
+            </div>
+            <a href="#contact" rel="nofollow" class="btn btn-blue mx-auto">Contact Us</a>
+        </div>
+    </section>
+    <section id="portfolio">
+        <div class="container text-center">
+            <h1 class="heading">
+                <span class="text-light-gray">This is our work</span>
+                <span class="clearfix"></span>
+                Portfolio
+            </h1>
+            <a href="#" class="btn btn-blue">See more</a>
+        </div>
+    </section>
+    <section id="team">
+        <div class="container text-center">
+            <h1 class="heading">
+                <span class="text-white">Be part of the team</span>
+                <span class="clearfix"></span>
+                Our Team
+            </h1>
+            <div class="row">
+                <div class="col-12 col-md-4 mx-auto">
+                    <div class="img-container">
+                        <img src="{{ asset('img/team/1.png') }}" alt="Manager">
+                        <a href="" class="absolut-align smooth-transition-300" rel="nofollow" target="_blank">
+                            <i class="fab fa-facebook-f absolut-align"></i>
+                        </a>
+                    </div>
+                    <h5>Manager</h5>
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="col-12 col-md-4 mx-auto">
+                    <div class="img-container">
+                        <img src="{{ asset('img/team/2.png') }}" alt="Manager">
+                        <a href="" class="absolut-align smooth-transition-300" rel="nofollow" target="_blank">
+                            <i class="fab fa-facebook-f absolut-align"></i>
+                        </a>
+                    </div>
+                    <h5>Manager</h5>
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="col-12 col-md-4 mx-auto">
+                    <div class="img-container">
+                        <img src="{{ asset('img/team/3.png') }}" alt="Manager">
+                        <a href="" class="absolut-align smooth-transition-300" rel="nofollow" target="_blank">
+                            <i class="fab fa-facebook-f absolut-align"></i>
+                        </a>
+                    </div>
+                    <h5>Manager</h5>
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <div class="container">
+            <h1 class="heading text-center">
+                <span class="text-light-gray">Get in touch</span>
+                <span class="clearfix"></span>
+                Contact
+            </h1>
+            <div class="relative-container">
+                <div class="w-50 contact-form">
+                    {!! Form::open(['url' => '/']) !!}
+                    <div class="w-50 float-left">
+                        {!! Form::label('name','Full Name') !!}
+                        {!! Form::text('name',null,['required' => 'required','placeholder' => 'Full Name']) !!}
+                    </div>
+                    <div class="w-50 float-left">
+                        {!! Form::label('phone','Phone') !!}
+                        {!! Form::tel('phone',null,['required' => 'required','placeholder' => '+52(614) 123 65 89']) !!}
+                    </div>
+                    <div>
+                        {!! Form::label('mail','Email') !!}
+                        {!! Form::email('mail',null,['required' => 'required','placeholder' => 'example@mail.com']) !!}
+                    </div>
+                    <div>
+                        {!! Form::label('message','Message') !!}
+                        {!! Form::textarea('message',null,['required' => 'required','placeholder' => 'Your message','rows' => '3']) !!}
+                    </div>
+                    <div class="text-right">
+                        {!! Form::submit('Send',['class' => 'btn btn-blue']) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.493826057543!2d-106.08218018491647!3d28.674870982401185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea4376f60fc76b%3A0xab78fb18d989c09!2sParque+de+Innovaci%C3%B3n+y+Transferencia+de+Tecnolog%C3%ADa%2C+Bodegas+del+Estado%2C+Chihuahua%2C+Chih.!5e0!3m2!1ses!2smx!4v1535937321992"
+                            width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,35 +5,41 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     @yield('head')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body data-spy="scroll" data-target="#navbar">
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top smooth-transition-300" id="navbar">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#banner"><img src="{{ asset('img/logo.png') }}" alt="NearMX"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <ul class="navbar-nav ml-auto mr-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#banner">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
+                    <a class="nav-link" href="#services">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Portfolio</a>
+                    <a class="nav-link" href="#portfolio">Portfolio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Our Team</a>
+                    <a class="nav-link" href="#team">Our Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-blue" href="#contact">Contact</a>
                 </li>
             </ul>
         </div>
@@ -42,7 +48,43 @@
 <div class="main" id="app">
     @yield('content')
 </div>
-@yield('scripts')
+<footer class="text-white">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-sm-5 col-md-4">
+                    <p class="mx-auto">
+                        Lorem ipsum #5698, Col. Lorem Ipsum
+                        <span class="clearfix"></span>
+                        Chihuahua, Chih.
+                    </p>
+                    <p class="mx-auto">
+                        Tel. +52 (614) 145 8963
+                        <span class="clearfix"></span>
+                        Cel. +52 (614) 156 9863
+                    </p>
+                    <p class="mx-auto">
+                        info@hotmail.com
+                    </p>
+                </div>
+                <div class="col-12 col-sm-7 col-md-8">
+                    <img src="{{ asset('img/logo-footer.png') }}" alt="NearMX" class="img-fluid mx-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <span>Derechos Reservados {{ date('Y') }}</span>
+    </div>
+</footer>
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 </body>
 </html>
